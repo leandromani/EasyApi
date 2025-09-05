0dcd9ac0b688194ad355fd6b33a1239824b54bfc
leandro.mani <leandro.mani@liip.ch>
2025-09-05 11:12:55 +0200

    feat: added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Easy Api",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Leandro Mani",
+    description="A simple example package",
+)
